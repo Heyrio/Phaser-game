11789aaee5bd78fad3b501e13e6db09cb92c67bc
Hamza Amer <hamer1563@gmail.com>
2019-05-13 17:49:51 -0400

    initial

---
+++ public/states/state1.js
@@ -0,0 +1,12 @@
+
+//basic template of a state
+
+var demo = {};
+
+demo.state1 = function(){};
+
+demo.state1.prototype = {
+    preload: function(){},
+    create: function(){},
+    update: function(){}
+}
