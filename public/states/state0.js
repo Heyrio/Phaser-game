ca65fe2f6a23a0ab93298c8b7efff6bfb90a6887
Hamza Amer <hamer1563@gmail.com>
2019-05-13 22:11:49 -0400

    Added sprites and player movement

--- public/states/state0.js
+++ public/states/state0.js
@@ -1,19 +1,30 @@
 var demo = {};
 demo.state0 = function(){};
-
+var centerX = 1500/2;
+var centerY = 1000/2;
+var eve;
+var speed = 4;
 demo.state0.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image('eve', '../assets/sprites/char1.png')
+    },
     create: function(){
         game.stage.backgroundColor = '#dddddd';
         console.log("you're in state0");
-        
+        eve = game.add.sprite(centerX, centerY, 'eve');
         addChangeStateEventListeners();
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        eve.anchor.setTo(0.5, 0.5);
     },
-    update: function(){}
+    update: function(){
+        if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            eve.x +=speed;
+        }else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            eve.x -=speed;
+
+        }
+    }
 }
-
-
 
 function changeState(i, stateNum){
     game.state.start('state'+ stateNum);
